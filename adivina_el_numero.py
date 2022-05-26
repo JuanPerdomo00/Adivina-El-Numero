26515d8a165015fc4e51b181870cb158519010dd
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-05-26 16:18:57 -0500

    mejorado

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,86 +1,107 @@
 #!/usr/bin/python3
 # create by: jakepy
 import random
+from color import Color as c
 import platform
 import time
 import os
 
-
+# ? limpia la pantalla dependiendo de el sistema operativo
 def clear():
-    if platform.system() == 'Linux':
-        os.system('clear')
-    else:
+    if platform.system() == 'nt':
         os.system('cls')
+    else:
+        os.system('clear')
 
 
+# ? crea el baner de presentacion en la terminal
 def banner():
     clear()
-    print('*' * 45)
-    print('   Bienvenido al juego adivina el numero')
-    print('*' * 45)
+    print(f'{c.y}*{c.off}' * 45)
+    print(f'   {c.v}Bienvenido al juego adivina el numero{c.off}')
+    print(f'{c.y}*{c.off}' * 45)
 
 
+# ? fumcion de vidas
 def vidas():
-    vida = str(input('Quieres elejir la cantidad de vidas si[S] no[N]: '))
-    banner()
-    if vida == 'si' or vida == 'S':
-        vida = int(input('Cuantas vidas quieres: '))
-        print(f'Iniciaras con {vida} vidas')
-        time.sleep(1.4)
-        print('Preparado!!!')
-        time.sleep(1.4)
-        print("GO!!!")
-        time.sleep(1.3)
+    try:
+        vida = str(input(f'{c.v}Quieres elejir la cantidad de vidas si{c.r}[S]{c.v} no{c.r}[N]{c.off}: '))
         banner()
-        return vida
+        if vida == 'si' or vida == 'S' or vida == 's':
+            vida = int(input('Cuantas vidas quieres: '))
+            print(f'Iniciaras con {vida} vidas')
+            time.sleep(1.4)
+            print(f'{c.v}Pre{c.r}par{c.y}ado{c.b}!!!{c.off}')
+            time.sleep(1.4)
+            print(f"{c.b}GO{c.v}!!{c.m}!{c.off}")
+            time.sleep(1.3)
+            banner()
+            return vida
 
-    elif vida == 'no' or vida == 'N':
-        vida = 10
-        print(f'Iniciaras con {vida} vidas por defecto')
-        time.sleep(1.5)
-        print('Preparado!!!')
-        time.sleep(1.5)
-        print("GO!!!")
-        time.sleep(1.3)
-        banner()
-        return vida
-    else:
-        print('Ingrese una opcion valida')
+        elif vida == 'no' or vida == 'N' or vida == 'n':
+            vida = 10
+            print(f'{c.v}Iniciaras con {c.r}{vida} {c.v}vidas por defecto{c.off}')
+            time.sleep(1.2)
+            banner()
+            time.sleep(1.5)
+            print(f'{c.v}Pre{c.r}par{c.y}ado{c.b}!!!{c.off}')
+            time.sleep(1.5)
+            print(f"{c.b}GO{c.v}!!{c.m}!{c.off}")
+            time.sleep(1.3)
+            banner()
+            return vida
+        else:
+            print(f'{c.r}[!] Ingrese una opcion valida {c.off}')
+            exit(1)
+
+    except KeyboardInterrupt as e:
+        print(e)
+        print(f'\n{c.f} [!] Salio forzadamente{c.off}')
         exit(1)
 
+
+# ? Funcion de logica principal
 def adivina_numero(n):
     vida = vidas()
     numero_random = random.randint(1, n)
-    numero_user = int(input('Ingresa un numero: '))
-
+    numero_user = int(input(f'\n{c.y}[?]{c.v} Ingresa un numero: {c.off}'))
+    banner()
     while numero_user != numero_random:
-        print('vidas: ' + ' ❤ ️' * vida + '\n')
-
+        banner()
+        print(f'{c.v}tienes {c.r}{vida}{c.off} {c.v}vidas: {c.off}' + f'{c.r} ❤ ️{c.off}' * vida + '\n')
         if vida == 0:
-            banner()
-            print(f'Perdiste vidas: {vida}')
-            print(f'El numero era: {numero_random}')
+            print(f'\n{c.v}Perdiste vidas: {c.r}{vida}{c.off}')
+            print(f'{c.v}El numero era: {c.r}{numero_random}{c.off}')
             break
 
+
         if numero_user < numero_random:
-            print('Busca un numero mas grande')
+            print(f'{c.v}Busca un numero mayor a {c.r}{numero_user}{c.off}')
             vida -= 1
         else:
-            print('Busca un numero mas pequeño')
+            print(f'{c.v}Busca un numero menor a {c.r}{numero_user}{c.off}')
             vida -= 1
-        numero_user = int(input('Ingresa otro numero: '))
+        numero_user = int(input('{}Ingresa otro numero: {}'.format(c.v, c.off)))
 
         if numero_user == numero_random:
             banner()
-            print('🥳 Ganaste el numero era: {} 🥳'.format(numero_random))
+            print('\n🥳 {} Ganaste el numero era: {} {}🥳\n'.format(c.m,c.off, numero_random))
             break
 
-
+# ? fUNCION PRINCIPAL ARRANCA EL PROGRAMA
 def main():
     banner()
-    n = int(input('Ingresa hasta que numero quieres adivinar: '))
-    banner()
-    adivina_numero(n)
+    try:
+        n = int(input(f'\n{c.b}Ingresa hasta que numero quieres adivinar{c.off} {c.y}->{c.off} '))
+        banner()
+        adivina_numero(n)
+    except ValueError as ve:
+        banner()
+        print('\n{} por favor ingrese algo. Crees que soy tonto? Sea serio {} {}XD {} '.format(c.r, c.off, c.y, c.off))
+    except KeyboardInterrupt:
+        banner()
+        print('\n{} Salio forzado. {}'.format(c.r, c.off))
+    
 
 
 if __name__ == '__main__':
