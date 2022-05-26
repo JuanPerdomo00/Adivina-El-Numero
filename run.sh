c30cec3b2bd3e127a8bb275823cc9a05ba8a540a
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-05-26 10:03:49 -0500

    Ejecutar

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# create by: jakepy
+
+function saludar() {
+    clear
+    echo "Descargando dependencias"
+    sleep 1
+    brew install cowsay
+    brew install lolcat
+    sleep 1
+    echo "Descargado :)"
+}
+
+
+function run() {
+    saludar
+    echo ''
+    sleep 1
+    cowsay -f tux "Dios ayudanos a mejorar cada dias mas." | lolcat
+    sleep 1
+
+    echo "Ejecutando juego" | lolcat
+    sleep 2
+    clear
+    python3 adivina_el_numero.py
+}
+
+run
